54539ce9f45e2cda72094e2907ab465b2677a3e6
Matt Lasko <matt.laszkiewicz@gmail.com>
2021-10-28 15:18:47 -0500

    implemented add and remove like functions likecontroller

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function add(){
+        $postId = (request('postId'));
+        $post = Post::find($postId)->get(['id','likes']);
+        $likes = $post->likes++;
+        $post::update([
+            'likes'=>$likes
+        ]);
+        return response(200);
+    }
+
+    public function remove(){
+        $postId = (request('postId'));
+        $post = Post::find($postId)->get(['id','likes']);
+        $likes = $post->likes--;
+        $post::update([
+            'likes'=>$likes
+        ]);
+        return response(200);
+    }
 }
