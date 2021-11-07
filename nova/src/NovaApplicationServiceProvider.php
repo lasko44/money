012f757f32c8f16a2942127c7e83ba04e838debe
Matt Lasko <matt.laszkiewicz@gmail.com>
2021-11-07 17:59:53 -0600

    added email

--- nova/src/NovaApplicationServiceProvider.php
+++ nova/src/NovaApplicationServiceProvider.php
@@ -68,6 +68,7 @@ class NovaApplicationServiceProvider extends ServiceProvider
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
                 //
+                'matt.laszkiewicz@gmail.com'
             ]);
         });
     }
