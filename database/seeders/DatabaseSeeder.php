e61b5d9d6e78800da7b772562d8a23e51fd36dd7
Matt Lasko <matt.laszkiewicz@gmail.com>
2021-11-18 21:10:45 -0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(BlogCategorySeeder::class);
-        $this->call(PostSeeder::class);
         $this->call(UserSeeder::class);
     }
 
